3cce48d967f9c3bc9b3b9bc94326653832a52b12
skorpion23 <94685729+skorpion23@users.noreply.github.com>
2022-05-31 23:46:20 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,13 +4,7 @@ document.querySelector('#menu-btn').onclick = () =>{
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
-let cartItem = document.querySelector('.cart-items-container');
 
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-}
 
 window.onscroll = () =>{
     navbar.classList.remove('active');
